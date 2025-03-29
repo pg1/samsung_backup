32473f87fb8fc75091acc2a3924e6792dc490515
Peter G <ujlevelek@gmail.com>
2025-03-29 08:18:37 +0100

    Added directory and file check

--- run.sh
+++ run.sh
@@ -1,12 +1,28 @@
 #!/bin/bash
 
+# Check if data directory exists and create it if not
+DATAPATH="data"
+if [ ! -d "$DATAPATH" ]; then
+	echo "Data directory does not exist. Creating it now..."
+	mkdir -p "$DATAPATH"
+	echo "Directory created: $DATAPATH"
+fi
+
+# Check if file.list is created
+if [ ! -f "file.list" ]; then
+	echo "ERROR: file.list is missing."
+	echo "- Create list using the following command: adb shell find /storage/ -name \"*.jpg\" > file.list"
+	exit
+fi
+
+#Get all files from the phone
 for i in `cat file.list`
 	do
-		FILE_NAME=$(basename "data/$i")
+		FILE_NAME=$(basename "$DATAPATH/$i")
 		#echo $i
-		if [ -f "$FILE_NAME" ]; then
+		if [ -f "$DATAPATH/$FILE_NAME" ]; then
 			echo "File exists: $FILE_NAME"
 		else
-			adb pull $i data/.
+			adb pull $i "$DATAPATH"/.
 		fi
 done
